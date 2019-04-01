43847398f91578c94162d47350f4a8caaae02c3e
slmnkhn79 <slmnkhn79@gmail.com>
2019-04-01 23:10:54 +0530

    Add files via Upload

--- Emp360/ClientApp/src/app/home/home.component.ts
+++ Emp360/ClientApp/src/app/home/home.component.ts
@@ -2,26 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import {EmployeeList } from '../search/IEmployeeSearch';
 import {HomeService} from './home.service';
 import { FormControl } from '@angular/forms';
-import { switchMap, debounceTime, tap, finalize } from 'rxjs/operators';
-import { Observable } from 'rxjs'
+import { switchMap } from 'rxjs/operators';
+import { debounceTime } from 'rxjs/internal/operators/debounceTime';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  queryField = new FormControl();
-  filteredOptions: Observable<EmployeeList>;
-
+  private results;
+  queryField: FormControl = new FormControl();
   constructor(private _homeService :HomeService) {}
    
 
   ngOnInit() {
-    this.filteredOptions = this.queryField.valueChanges
-      .pipe(
-        debounceTime(200),
-        switchMap(value => this._homeService.search(this.queryField.value))
-      );
+    this.queryField
+    .valueChanges
+  .pipe(
+    switchMap(value => this._homeService.search(this.queryField.value))
+    
+    );
     
  
   }
