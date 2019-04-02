ed2ea8c9855f3b1219a61bc4d9ce74cfabdf0651
slmnkhn79 <slmnkhn79@gmail.com>
2019-04-02 10:50:14 +0530

    Revert "Merge pull request #1 from slmnkhn79/TestBranchTwo"
    This reverts commit 02339fe0c9c0717ef85fa4b322ba8f1fffad5197, reversing
    changes made to 882ef91c4e21ebda7411b78c06b8fe9d21a25513.

--- Emp360/ClientApp/src/app/home/home.component.ts
+++ Emp360/ClientApp/src/app/home/home.component.ts
@@ -2,27 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import {EmployeeList } from '../search/IEmployeeSearch';
 import {HomeService} from './home.service';
 import { FormControl } from '@angular/forms';
-import { switchMap } from 'rxjs/operators';
-import { debounceTime } from 'rxjs/internal/operators/debounceTime';
-
+import { switchMap, debounceTime, tap, finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  private results;
-  queryField: FormControl = new FormControl();
+  queryField = new FormControl();
+  filteredOptions: Observable<EmployeeList>;
+
   constructor(private _homeService :HomeService) {}
    
 
   ngOnInit() {
-    this.queryField
-    .valueChanges
-  .pipe(
-    switchMap(value => this._homeService.search(this.queryField.value))
-    
-    );
+    this.filteredOptions = this.queryField.valueChanges
+      .pipe(
+        debounceTime(200),
+        switchMap(value => this._homeService.search(this.queryField.value))
+      );
     
  
   }
